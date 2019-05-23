508b68fd923b400794241b3cade3b0af54a7886f
njugunawairimu <susanwairimu699@gmail.com>
2019-05-23 08:36:58 +0300

    first commit

---
+++ current/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Event(models.Model):
+    banner = models.ImageField()
+    event_name = models.CharField(max_length=40)
+    venue = models.CharField(max_length=40)
+    date = models.DateField()
+    time = models.TimeField()
+    charges = models.CharField(max_length=40)
+    contact = models.IntegerField()
+
+    def __str__(self):
+        return self.event_name
+
+class Comment(models.Model):
+    event_name = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user_name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.user_name
---
+++ current/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Event
+
+# Create your views here.
+
+def index(request):
+    events = Event.objects.all()
+    context = {
+        'events': events
+    }
+    return render(request, 'current/index.html', context)
+
+
